8e86eabf2db79ff9587672344fd03edc79182268
秋雨落 <i@rain.cx>
2024-05-20 20:39:10 +0800

    :sparkles: Command.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- CatMessenger.Telegram/Bot/UpdateHandler.cs
+++ CatMessenger.Telegram/Bot/UpdateHandler.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace CatMessenger.Telegram.Bot;
 
@@ -15,25 +16,22 @@ public class UpdateHandler(
     RabbitMqConnector connector)
     : IUpdateHandler
 {
+    private string? Id { get; set; }
+    
     public async Task HandleUpdateAsync(ITelegramBotClient _, Update update, CancellationToken cancellationToken)
     {
-        // if (update.Type == UpdateType.Message 
-        //     && update.Message!.Type == MessageType.Text 
-        //     && update.Message.Text!.StartsWith("/"))
-        // {
-        //     var command = update.Message.Text.Substring(0, 1).Split(" ");
-        //     logger.LogInformation("Telegram command: {Command}", update.Message.Text);
-        //     await OnCommand(update.Message, command[0].Split('@')[0], command[1..]);
-        // }
-        // else
-        // {
-        //     var message = MessageParser.FromUpdate(update);
-        //     logger.LogInformation("Telegram message: {Message}", message.ToString());
-        //     connector.SendChatMessage(new ChatComponentPayload(message.ToString()));
-        // }
-
         if (update.Message != null && update.Message.Chat.Id != config.GetTelegramChatId())
         {
+            return;
+        }
+        
+        if (update.Type == UpdateType.Message 
+            && update.Message!.Type == MessageType.Text 
+            && update.Message.Text!.StartsWith('/'))
+        {
+            var command = update.Message.Text[1..].Split(" ");
+            logger.LogInformation("Telegram command: {Command}", update.Message.Text);
+            await OnCommand(update.Message, command[0], command[1..]);
             return;
         }
         
@@ -53,49 +51,56 @@ public class UpdateHandler(
         return Task.CompletedTask;
     }
 
-    public Task OnCommand(Message message, string command, params string[] args)
+    public async Task OnCommand(Message message, string command, params string[] args)
     {
+        Id ??= (await bot.GetMeAsync()).Username!;
         
-        // if (command == "online")
-        // {
-        //     if (args.Length != 1)
-        //     {
-        //         await bot.SendTextMessageAsync(message.Chat.Id, "用法不正确。/online <服务器名>", 
-        //             replyToMessageId: message.MessageId);
-        //         return;
-        //     }
-        //     
-        //     bot.QueryRequestOnline(message.MessageId, args[0]);
-        //     connector.SendChatMessage(new QueryOnlinePayload(args[0]));   // Todo: qyl27: When server is not exists?
-        //     return;
-        // }
-        //
-        // if (command == "time")
-        // {
-        //     if (args.Length is > 3 or < 1)
-        //     {
-        //         await bot.SendTextMessageAsync(message.Chat.Id, "用法不正确！/time <服务器名> [世界名] [查询类型]", 
-        //             replyToMessageId: message.MessageId);
-        //         return;
-        //     }
-        //
-        //     var world = args.Length >= 2 ? args[1] : "world";
-        //
-        //     var typeStr = args.Length == 3 ? args[2] : "DayTime";
-        //     var type = TelegramBotClientExtension.TimeQueryType.DayTime;
-        //     if (typeStr == "GameTime")
-        //     {
-        //         type = TelegramBotClientExtension.TimeQueryType.GameTime;
-        //     }
-        //     else if (typeStr == "Day")
-        //     {
-        //         type = TelegramBotClientExtension.TimeQueryType.Day;
-        //     }
-        //     
-        //     bot.QueryRequestTime(message.MessageId, args[0], world, type);
-        //     connector.SendChatMessage(new QueryTimePayload(args[0], world));   // Todo: qyl27: When server or world is not exists?
-        //     return;
-        // }
-        return Task.CompletedTask;
+        if (command.Contains('@'))
+        {
+            var sp = command.Split('@');
+            if (sp[1] != Id)
+            {
+                return;
+            }
+        }
+
+        if (command.StartsWith("online"))
+        {
+            if (args.Length != 1)
+            {
+                await bot.SendTextMessageAsync(message.Chat.Id, "用法不正确！\n/online <服务器名>", 
+                    replyToMessageId: message.MessageId);
+                return;
+            }
+
+            await connector.Publish(new ConnectorCommand
+            {
+                Command = ConnectorCommand.EnumCommand.QueryOnline,
+                Client = args[0]
+            });
+            await bot.SendTextMessageAsync(message.Chat.Id, "查询中，请稍候。", 
+                replyToMessageId: message.MessageId);
+        }
+        else if (command.StartsWith("time"))
+        {
+            if (args.Length is > 3 or < 1)
+            {
+                await bot.SendTextMessageAsync(message.Chat.Id, "用法不正确！\n/time <服务器名> [世界名] [查询类型]", 
+                    replyToMessageId: message.MessageId);
+                return;
+            }
+            
+            var world = args.Length >= 2 ? args[1] : "world";
+            var typeStr = args.Length >= 3 ? args[2] : "DayTime";
+            
+            await connector.Publish(new ConnectorCommand
+            {
+                Command = ConnectorCommand.EnumCommand.QueryWorldTime,
+                Client = args[0], 
+                Arguments = [world, typeStr]
+            });
+            await bot.SendTextMessageAsync(message.Chat.Id, "查询中，请稍候。", 
+                replyToMessageId: message.MessageId);
+        }
     }
 }
